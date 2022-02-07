e8c6cdd2f34dfcd4803244597f8c01f152c862b6
mona <csimonusa@outlook.com>
2022-02-07 15:32:08 -0500

    added crud routes to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,54 @@
 const router = require('express').Router();
+const { Model } = require('sequelize/types');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [{
+      Model: Product,
+      through: ProductTag
+    }]
+  }).then((tags) => res.json(tags))
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Product,
+      through: ProductTag
+    }]
+  }).then((tag) => res.json(tag))
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
+  Tag.create(req.body).then((tag) => res.json(tag))
   // create a new tag
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then((tag) => res.json(tag))
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((tag) => res.json(tag))
   // delete on tag by its `id` value
 });
 
