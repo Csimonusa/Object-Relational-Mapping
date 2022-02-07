77f8a0aee9faccb6f1d64fd71de18f50a988f45b
mona <csimonusa@outlook.com>
2022-02-07 15:14:10 -0500

    added category crud routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,24 +4,44 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include: [Product],
+  }).then((categories) => res.json(categories))
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product],
+  }).then((category) => res.json(category))
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create(req.body).then((category) => res.json(category))
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  }).then((category) => res.json(category))
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    },
+  }).then((category) => res.json(category))
   // delete a category by its `id` value
 });
 
